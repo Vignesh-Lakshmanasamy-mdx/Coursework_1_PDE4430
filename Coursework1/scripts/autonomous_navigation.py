a28c1cd91d0a7d4a92a7ebe88d7adae6f071747f
Vignesh-Lakshmanasamy-mdx <vl223@live.mdx.ac.uk>
2024-11-17 12:28:33 -0800

    Task2 Started -UI created

---
+++ Coursework1/scripts/autonomous_navigation.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import rospy
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+from std_srvs.srv import Empty
+import sys
+
+
+def main():
+   
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+
+
+
+# Form implementation generated from reading ui file 'Task2.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(640, 480)
+
+        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+        self.buttonBox.setGeometry(QtCore.QRect(10, 440, 621, 32))
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+
+        self.textBrowser_5 = QtWidgets.QTextBrowser(Dialog)
+        self.textBrowser_5.setGeometry(QtCore.QRect(20, 30, 601, 81))
+        self.textBrowser_5.setObjectName("textBrowser_5")
+
+        self.textEdit_3 = QtWidgets.QTextEdit(Dialog)
+        self.textEdit_3.setGeometry(QtCore.QRect(20, 130, 601, 301))
+        self.textEdit_3.setObjectName("textEdit_3")
+
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(30, 210, 171, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label.setFont(font)
+        self.label.setAutoFillBackground(True)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(30, 260, 171, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_2.setFont(font)
+        self.label_2.setAutoFillBackground(True)
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setGeometry(QtCore.QRect(30, 310, 171, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_3.setFont(font)
+        self.label_3.setAutoFillBackground(True)
+        self.label_3.setObjectName("label_3")
+        self.textEdit_4 = QtWidgets.QTextEdit(Dialog)
+        self.textEdit_4.setGeometry(QtCore.QRect(210, 170, 191, 191))
+        self.textEdit_4.setObjectName("textEdit_4")
+        self.textEdit_5 = QtWidgets.QTextEdit(Dialog)
+        self.textEdit_5.setGeometry(QtCore.QRect(410, 170, 191, 191))
+        self.textEdit_5.setObjectName("textEdit_5")
+
+        self.origin_x = QtWidgets.QTextEdit(Dialog)
+        self.origin_x.setGeometry(QtCore.QRect(220, 210, 171, 31))
+        font = QtGui.QFont()
+        font.setFamily("URW Bookman")
+        font.setPointSize(11)
+        font.setBold(True)
+        font.setWeight(75)
+        self.origin_x.setFont(font)
+        self.origin_x.setObjectName("origin_x")
+        self.Origin_y = QtWidgets.QTextEdit(Dialog)
+        self.Origin_y.setGeometry(QtCore.QRect(220, 260, 171, 31))
+        font = QtGui.QFont()
+        font.setFamily("URW Bookman")
+        font.setPointSize(11)
+        font.setBold(True)
+        font.setWeight(75)
+        self.Origin_y.setFont(font)
+        self.Origin_y.setObjectName("Origin_y")
+        
+        self.origin_theta = QtWidgets.QTextEdit(Dialog)
+        self.origin_theta.setGeometry(QtCore.QRect(220, 310, 171, 31))
+        font = QtGui.QFont()
+        font.setFamily("URW Bookman")
+        font.setPointSize(11)
+        font.setBold(True)
+        font.setWeight(75)
+        self.origin_theta.setFont(font)
+        self.origin_theta.setObjectName("origin_theta")
+        
+        self.Target_Theta = QtWidgets.QTextEdit(Dialog)
+        self.Target_Theta.setGeometry(QtCore.QRect(420, 310, 171, 31))
+        font = QtGui.QFont()
+        font.setFamily("URW Bookman")
+        font.setPointSize(11)
+        font.setBold(True)
+        font.setWeight(75)
+        self.Target_Theta.setFont(font)
+        self.Target_Theta.setObjectName("Target_Theta")
+        
+        self.Target_x = QtWidgets.QTextEdit(Dialog)
+        self.Target_x.setGeometry(QtCore.QRect(420, 210, 171, 31))
+        font = QtGui.QFont()
+        font.setFamily("URW Bookman")
+        font.setPointSize(11)
+        font.setBold(True)
+        font.setWeight(75)
+        self.Target_x.setFont(font)
+        self.Target_x.setObjectName("Target_x")
+        
+        self.Target_y = QtWidgets.QTextEdit(Dialog)
+        self.Target_y.setGeometry(QtCore.QRect(420, 260, 171, 31))
+        font = QtGui.QFont()
+        font.setFamily("URW Bookman")
+        font.setPointSize(11)
+        font.setBold(True)
+        font.setWeight(75)
+        self.Target_y.setFont(font)
+        self.Target_y.setObjectName("Target_y")
+        
+        self.textBrowser = QtWidgets.QTextBrowser(Dialog)
+        self.textBrowser.setGeometry(QtCore.QRect(40, 135, 561, 31))
+        self.textBrowser.setObjectName("textBrowser")
+        
+        self.move_to_target = QtWidgets.QPushButton(Dialog)
+        self.move_to_target.setGeometry(QtCore.QRect(220, 380, 171, 25))
+        self.move_to_target.setObjectName("move_to_target")
+        
+        self.Rest_turtle = QtWidgets.QPushButton(Dialog)
+        self.Rest_turtle.setGeometry(QtCore.QRect(420, 380, 171, 25))
+        self.Rest_turtle.setObjectName("Rest_turtle")
+
+        self.retranslateUi(Dialog)
+        self.buttonBox.accepted.connect(Dialog.accept)
+        self.buttonBox.rejected.connect(Dialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.textBrowser_5.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'Ubuntu\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">PDE4430 - Mobile Robotics</span></p>\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Coursework 1 -  Task 2</p>\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600; color:#4e9a06;\">Autonomous navigation to any given coordinate in the Turtlesim window</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
+        self.label.setText(_translate("Dialog", "X coordinate point"))
+        self.label_2.setText(_translate("Dialog", "Y coordinate point"))
+        self.label_3.setText(_translate("Dialog", "Theta Value"))
+        self.textEdit_4.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'Ubuntu\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Turtle Origin Coordinates</p></body></html>"))
+        self.textEdit_5.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'Ubuntu\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Turtle Target Coordinates</p></body></html>"))
+        self.origin_x.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'URW Bookman\'; font-size:11pt; font-weight:600; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'Ubuntu\'; font-weight:400;\"><br /></p></body></html>"))
+        self.Origin_y.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'URW Bookman\'; font-size:11pt; font-weight:600; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'Ubuntu\'; font-weight:400;\"><br /></p></body></html>"))
+        self.origin_theta.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'URW Bookman\'; font-size:11pt; font-weight:600; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'Ubuntu\'; font-weight:400;\"><br /></p></body></html>"))
+        self.Target_Theta.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'URW Bookman\'; font-size:11pt; font-weight:600; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'Ubuntu\'; font-weight:400;\"><br /></p></body></html>"))
+        self.Target_x.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'URW Bookman\'; font-size:11pt; font-weight:600; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'Ubuntu\'; font-weight:400;\"><br /></p></body></html>"))
+        self.Target_y.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'URW Bookman\'; font-size:11pt; font-weight:600; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'Ubuntu\'; font-weight:400;\"><br /></p></body></html>"))
+        self.textBrowser.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'Ubuntu\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Enter X &amp; Y coordinate value Between 1 &amp; 10</p></body></html>"))
+        self.move_to_target.setText(_translate("Dialog", "Move to Target"))
+        self.Rest_turtle.setText(_translate("Dialog", "Reset Turtle Window"))
+
+
+if __name__ == "__main__":
+    main()
